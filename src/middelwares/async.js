961976f67dcc6e16e0b72206da0b310954eab2de
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-01-13 10:11:17 +0100

    handling promise

--- src/middelwares/async.js
+++ src/middelwares/async.js
@@ -1,7 +1,18 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // If action does not have payload or the payload does not have a .then property
+    // we don't care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    next(action);
+    // make sure the action's promise resolves
+    action.payload
+      .then(function(response) {
+        // create a new action with the old type
+        // but replace the promise with the response data
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+      });
   }
 }
